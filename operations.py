80c033b905136e713de6e160cc8783be05db86a4
Junior <miguel.ajr89@gmail.com>
2024-08-13 02:23:33 +0000

    Example of operations with integers, floats and booleans

---
+++ operations.py
@@ -0,0 +1,28 @@
+# Basic Operations
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Substraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Mod:", a % b)
+print("Double Division:", a // b)
+print("Empowerment:", a ** b)
+
+# Shortcuts
+a += 2
+print(a)
+a -= 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+
+# Boolean operations
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
