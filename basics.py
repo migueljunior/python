f4f5cbf09b69b032655d0fea360789119aa2af71
Junior <miguel.ajr89@gmail.com>
2024-08-13 01:59:21 +0000

    Operations with Strings

--- basics.py
+++ basics.py
@@ -4,3 +4,27 @@ print("Hey, I'm a message in your console!")
 # Variables
 name = "Junior"
 print("Hey, I'm showing the next variable: " + name)
+
+# String type
+# To ask the type of variable we can use the function type
+type_name = str(type(name))
+print("The variable name with value " + name + " is type " + type_name)
+
+# You can declare String in the next three modes
+mode_1 = "Hi"
+mode_2 = 'Hello again'
+mode_3 = '''This is the last time I say hello'''
+
+# We can use the index of String similar as an array
+testing = "I am a long String"
+print(testing[3])
+print(testing[5])
+print(testing[-1])
+print(testing[-6])
+
+# Replicate the string 5 times
+print(5 * mode_1)
+
+# Count the characters
+print(len(mode_3))
+print(len(mode_1))
