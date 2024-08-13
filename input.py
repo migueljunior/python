d31de13fc6484968e5c75e212d3e25bacdd5d111
Junior <miguel.ajr89@gmail.com>
2024-08-13 02:30:30 +0000

    Using input

---
+++ input.py
@@ -0,0 +1,8 @@
+# Asking for information to user
+name = input("Enter your name: ")
+print(name)
+print(type(name))
+# Doing a casting to get a integer
+age = int(input("Enter your age: "))
+print(age)
+print(type(age))
